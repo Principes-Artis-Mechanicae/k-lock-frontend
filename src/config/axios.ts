7a24f5770b30dc049cbc74d4df202ed1ae29a2d3
bbong8 <ghdqhdrl612@knu.ac.kr>
2024-09-23 03:52:17 +0900

    fix: axios 설정 변경

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
-
 export const api = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: "/api",
     headers: {
         "Content-Type": "application/json",
     },
+    withCredentials: true,
 });
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,15 @@ export default defineConfig({
     resolve: {
         alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
     },
+    server: {
+        proxy: {
+            "/api": {
+                target: process.env.VITE_API_URL,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
+    },
     plugins: [
         react({
             jsxImportSource: "@emotion/react",
