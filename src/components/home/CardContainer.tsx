7ee85c948c7f5cc3646e8d06b712e06b2188dcc4
bbong8 <ghdqhdrl612@knu.ac.kr>
2024-09-25 01:33:22 +0900

    feat: card duration 메모이제이션 최적화

--- src/components/home/CardContainer.tsx
+++ src/components/home/CardContainer.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useSelector } from "react-redux";
 
 import { addOneDay, dateValueFormatter } from "@/utils/utils";
@@ -10,45 +11,49 @@ const CardContainer = () => {
     const { firstApplyStartDate, firstApplyEndDate, semesterEndDate, dateType } = useSelector(
         (state: RootState) => state.period,
     );
-    const additionalApplyStartDate = addOneDay(firstApplyEndDate);
-    const cardData = [
-        {
-            duration:
-                dateType === "additionalApply"
-                    ? `${dateValueFormatter(additionalApplyStartDate)} - ${dateValueFormatter(semesterEndDate)}`
-                    : `${dateValueFormatter(firstApplyStartDate)} - ${dateValueFormatter(firstApplyEndDate)}`,
-            title: dateType === "additionalApply" ? "사물함 추가 신청" : "사물함 신청",
-            description: "KLOCKER Application & Additional Application",
-            navigationPath: "/apply",
-        },
-        {
-            duration: `${dateValueFormatter(additionalApplyStartDate)} - ${dateValueFormatter(semesterEndDate)}`,
-            title: "사물함 배정 조회",
-            description: "KLOCKER Assignment Information",
-            navigationPath: "/check",
-        },
-        {
-            duration: `${dateValueFormatter(additionalApplyStartDate)} - ${dateValueFormatter(semesterEndDate)}`,
-            title: "사물함 고장 신고 및 변경 신청",
-            description: "KLOCKER Malfunction & Change Request",
-            navigationPath: "/report",
-        },
-    ];
+
+    const additionalApplyStartDate = useMemo(() => addOneDay(firstApplyEndDate), [firstApplyEndDate]);
+
+    const cardData = useMemo(() => {
+        const applyDuration =
+            dateType === "additionalApply"
+                ? `${dateValueFormatter(additionalApplyStartDate)} - ${dateValueFormatter(semesterEndDate)}`
+                : `${dateValueFormatter(firstApplyStartDate)} - ${dateValueFormatter(firstApplyEndDate)}`;
+
+        return [
+            {
+                duration: applyDuration,
+                title: dateType === "additionalApply" ? "사물함 추가 신청" : "사물함 신청",
+                description: "KLOCKER Application & Additional Application",
+                navigationPath: "/apply",
+            },
+            {
+                duration: `${dateValueFormatter(additionalApplyStartDate)} - ${dateValueFormatter(semesterEndDate)}`,
+                title: "사물함 배정 조회",
+                description: "KLOCKER Assignment Information",
+                navigationPath: "/check",
+            },
+            {
+                duration: `${dateValueFormatter(additionalApplyStartDate)} - ${dateValueFormatter(semesterEndDate)}`,
+                title: "사물함 고장 신고 및 변경 신청",
+                description: "KLOCKER Malfunction & Change Request",
+                navigationPath: "/report",
+            },
+        ];
+    }, [additionalApplyStartDate, firstApplyStartDate, firstApplyEndDate, semesterEndDate, dateType]);
 
     return (
-        <>
-            <CardContainerWrapper>
-                {cardData.map((card, index) => (
-                    <Card
-                        key={index}
-                        duration={card.duration}
-                        title={card.title}
-                        description={card.description}
-                        navigationPath={card.navigationPath}
-                    />
-                ))}
-            </CardContainerWrapper>
-        </>
+        <CardContainerWrapper>
+            {cardData.map((card, index) => (
+                <Card
+                    key={index}
+                    duration={card.duration}
+                    title={card.title}
+                    description={card.description}
+                    navigationPath={card.navigationPath}
+                />
+            ))}
+        </CardContainerWrapper>
     );
 };
 
